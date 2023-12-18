f572395b2b1dc8bc7ecc3f8e50a50d5020c2777b
Your Name <your.email@example.com>
2023-12-19 01:56:35 +0530

    done

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,39 +1,46 @@
 export interface Order {
-  price: Number,
-  size: Number,
+  price: number;
+  size: number;
 }
-/**
- * The datafeed server returns an array of ServerRespond with 2 stocks.
- * We do not have to manipulate the ServerRespond for the purpose of this task.
- */
+
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
-  timestamp: Date,
+  stock: string;
+  top_bid: Order;
+  top_ask: Order;
+  timestamp: Date;
 }
 
 class DataStreamer {
-  // The url where datafeed server is listening
   static API_URL: string = 'http://localhost:8080/query?id=1';
 
-  /**
-   * Send request to the datafeed server and executes callback function on success
-   * @param callback callback function that takes JSON object as its argument
-   */
-  static getData(callback: (data: ServerRespond[]) => void): void {
-    const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+  static getData(): Promise<ServerRespond[]> {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      const timeoutDuration = 10000; // 10 seconds timeout
 
-    request.onload = () => {
-      if (request.status === 200) {
-        callback(JSON.parse(request.responseText));
-      } else {
-        alert ('Request failed');
-      }
-    }
+      request.open('GET', DataStreamer.API_URL, true);
+      request.timeout = timeoutDuration;
 
-    request.send();
+      request.onload = () => {
+        if (request.status === 200) {
+          resolve(JSON.parse(request.responseText));
+        } else {
+          reject(new Error(`Request failed with status: ${request.status}`));
+        }
+      };
+
+      request.onerror = () => {
+        console.error('Network error occurred');
+        reject(new Error('Network error occurred'));
+      };
+
+      request.ontimeout = () => {
+        console.error('Request timed out');
+        reject(new Error('Request timed out'));
+      };
+
+      request.send();
+    });
   }
 }
 
